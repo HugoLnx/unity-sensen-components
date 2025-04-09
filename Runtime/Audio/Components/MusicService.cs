48825b3cfaa5fee02997641dee33e08ea533d4da
hugolnx <hugolnx@gmail.com>
2025-04-09 18:34:38 -0300

    boot player on MusicService

--- Runtime/Audio/Components/MusicService.cs
+++ Runtime/Audio/Components/MusicService.cs
@@ -3,5 +3,16 @@ using UnityEngine;
 namespace Sensen.Components
 {
     public class MusicService : AudioPlayerBase<MusicService>
-    {}
+    {
+        [SerializeField] private AudioProfile _bootProfile;
+
+        protected override void AwakeSingleton()
+        {
+            base.AwakeSingleton();
+            if (_bootProfile != null)
+            {
+                Play(_bootProfile);
+            }
+        }
+    }
 }
